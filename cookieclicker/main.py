0bb75c705b00e8f2604530f5a65e3f7fa074c9f0
virotheo <virotheo78@gmail.com>
2026-05-30 21:10:34 +0200

    Implement auto-save and upgrade options
    Added auto-save functionality and new upgrade buttons.

--- cookieclicker/main.py
+++ cookieclicker/main.py
@@ -1,23 +1,42 @@
 from tkinter import *
 import requests
 import webbrowser
+import os
 
 ip = requests.get("https://api.ipify.org").text
-version = 1.0
+version = 1.1
 coins = 0
+
+# creation sauvegarde si pas présent
+
+if os.path.exists("save.txt"):
+      with open("save.txt", "r") as saving:
+       try:
+            coins = int(saving.read())
+       except:
+            coins = 0
+       saving.close()
+else:
+     with open("save.txt", "w+") as save:
+      save.write("0")
+      save.close()
 
 # On définit un gestionnaire d'événements pour le bouton ci-dessous.
 def farm():
     global coins
-    coins+=5
+    coins += 5
     coins_show.config(text=coins)
 
 def link_support():
         webbrowser.open_new("https://discord.gg/upM5gf9RxM")
 
 def save():
-      print("saved!")        
+      with open("save.txt", "w") as saving:
+            saving.write(str(coins))
 
+def auto_save():
+      save()
+      window.after(1000, auto_save)
 
 # On instancie notre fenêtre graphique
 window = Tk()
@@ -37,12 +56,24 @@ label_title.pack()
 footer_ip = Label(window, text=f'Your IP : {ip}', font=('courrier', 8  ),bg='#91370d', fg='#ffffff')
 footer_ip.pack(side=BOTTOM)
 
-coins_show = Label(window, text=coins, font=('courrier', 8  ),bg='#91370d', fg='#ffffff')
+coins_show = Label(frame, text=coins, font=('courrier', 8  ),bg='#91370d', fg='#ffffff')
 coins_show.pack()
 coins_show.config(text=coins)
 
-farm_button = Button(frame, text='Farm', font=('Courrier', 25),bg='#ffffff', fg='#91370d', command=farm)
-farm_button.pack(pady=25, fill=X)
+upgrade_1 = Button(frame, text='Buy a Kitchen (x1.25)', font=('Courrier', 5),bg='#ffffff', fg='#91370d', command=None)
+upgrade_1.pack(pady=5,side=BOTTOM)
+
+upgrade_2 = Button(frame, text='Buy a Furnace (x1.5)', font=('Courrier', 5),bg='#ffffff', fg='#91370d', command=None)
+upgrade_2.pack(pady=5,side=BOTTOM)
+
+upgrade_3 = Button(frame, text='Upgrade Furnace (x1.75)', font=('Courrier', 5),bg='#ffffff', fg='#91370d', command=None)
+upgrade_3.pack(pady=5,side=BOTTOM)
+
+upgrade_4 = Button(frame, text='Buy Vanilla Cookie Method (x2)', font=('Courrier', 5),bg='#ffffff', fg='#91370d', command=None)
+upgrade_4.pack(pady=5,side=BOTTOM)
+
+farm_button = Button(frame, text='Farm', font=('Courrier', 15),bg='#ffffff', fg='#91370d', command=farm)
+farm_button.pack(pady=15)
 
 support = Button(window, text='Support Discord', font=('Courrier', 10),bg='#ffffff', fg='#91370d', command=link_support)
 support.pack(side=BOTTOM)
@@ -56,4 +87,7 @@ menu_bar.add_cascade(label="Fichier", menu=file_menu)
 
 window.config(menu=menu_bar)
 frame.pack()
+
+auto_save()
+
 window.mainloop()
